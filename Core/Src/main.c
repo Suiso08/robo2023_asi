3663c8729864054bd0a079c6558ef82d4115dca7
Suiso08 <85001496+Suiso08@users.noreply.github.com>
2025-06-10 16:19:40 +0900

    Add files via upload

---
+++ Core/Src/main.c
@@ -0,0 +1,570 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim2;
+
+UART_HandleTypeDef huart1;
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM2_Init(void);
+static void MX_USART1_UART_Init(void);
+static void MX_USART2_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+//uint8_t b;//ス????��?��??��?��???��?��??��?��?ィ????��?��??��?��???��?��??��?��?クのx軸とy軸とボタン
+#define RX_BUFFER_SIZE 64
+uint8_t rxBuffer[RX_BUFFER_SIZE];
+uint32_t rxIndex = 0;
+char string[RX_BUFFER_SIZE];
+
+void copy()
+{
+	for(int i=0;i<RX_BUFFER_SIZE;i++)
+	{
+		string[i]=rxBuffer[i];
+	}
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+  if (huart == &huart1)
+  {
+	  if (rxBuffer[rxIndex - 1] == '\r')
+	  {
+		  //改行が受信された�????��?��??��?��合�????��?��??��?��受信された文字を処???��?��??��?��?
+		  rxBuffer[rxIndex] = '\0';
+		  //HAL_UART_Transmit(&huart2, rxBuffer, rxIndex, HAL_MAX_DELAY);
+		  copy(); //バッファから???��?��??��?��?字�????��?��??��?��をコピ�????��?��??��?��
+		  rxIndex = 0;
+		  memset(rxBuffer, '\0', RX_BUFFER_SIZE); //バッファをクリア
+	  }
+	  //割り込みで受信
+	  HAL_UART_Receive_IT(&huart1, &rxBuffer[rxIndex], 1);
+	  rxIndex++;
+  }
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+	int i,j=0;
+	float r=0,arg=0;
+	int dash,above,gather;
+	char moji[RX_BUFFER_SIZE];
+	char test[32];
+	char w1[7],w2[7],w3[2],w4[2],w5[3];
+
+	int reset;
+	char check[RX_BUFFER_SIZE];
+	memset(check,'\0',RX_BUFFER_SIZE);
+
+	float x=0.0,y=0.0;
+	float spd=0.0,rad=0.0;
+	int T=0;
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM2_Init();
+  MX_USART1_UART_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+  if(HAL_TIM_PWM_Start(&htim2,TIM_CHANNEL_1) != HAL_OK){Error_Handler();}
+  if(HAL_TIM_PWM_Start(&htim2,TIM_CHANNEL_2) != HAL_OK){Error_Handler();}
+  HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6,GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,GPIO_PIN_RESET);
+  __HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_1,0);
+  __HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_2,0);
+
+  HAL_UART_Receive_IT(&huart1, rxBuffer, 1);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+
+	  //???��?��??��?��?字�????��?��??��?��???��?��??��?��????��?��??��?��初期???��?��??��?��?
+	  memset(moji,'\0',RX_BUFFER_SIZE);
+	  memset(test,'\0',32);
+	  memset(w1,'\0',7);
+	  memset(w2,'\0',7);
+	  memset(w3,'\0',2);
+	  memset(w4,'\0',2);
+	  memset(w5,'\0',3);
+	  //通信を開???��?��??��?��?
+	  HAL_UART_Receive_IT(&huart1, rxBuffer, 1);
+	  //受信した???��?��??��?��?字�????��?��??��?��を整???��?��??��?��?
+	  for(i=0;i<RX_BUFFER_SIZE;i++)//
+	  {
+		  moji[j]=string[i];
+		  if(moji[j] == '\r')
+		  {
+			  j=0;
+			  break;
+		  }
+		  else if(moji[j] != '\0')
+		  {
+			  j++;
+		  }
+	  }
+	  //整???��?��??��?��?した???��?��??��?��?字�????��?��??��?��から???��?��??��?��????��?��??��?��を抽出
+	  for(i=0;i<6;i++)
+	  {
+		  w1[i]=moji[2+i];
+		  w2[i]=moji[13+i];
+	  }
+	  w3[0]=moji[25];
+	  w4[0]=moji[33];
+	  for(i=0;i<2;i++)
+	  {
+		  w5[i]=moji[42+i];
+	  }
+	  r = atoff(w1);
+	  arg = atoff(w2);
+	  dash = atoi(w3);
+	  above = atoi(w4);
+	  gather = atoi(w5);
+
+	  //ここから実際の処???��?��??��?��?
+
+	  //???��?��??��?��?字�????��?��??��?��にして表示
+	  //snprintf(test,32,"%6.1f,%6.1f,%d,%d,%2d\n\r",r,arg,dash,above,gather);
+	  //HAL_UART_Transmit(&huart2, (uint8_t*)test, 32,HAL_MAX_DELAY);
+	  //HAL_UART_Transmit(&huart2, (uint8_t*)moji, RX_BUFFER_SIZE,HAL_MAX_DELAY);
+	  //HAL_UART_Transmit(&huart2, (uint8_t*)w5, 3, HAL_MAX_DELAY);
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	rad = arg*(M_PI/180.0);
+	x=r*cos(rad);
+	y=r*sin(rad);
+	if(isfinite(r)==0||r>1600)
+	{
+		r=1600;
+	}
+	//b=dash;
+
+	snprintf(test,32,"%7.1f,%7.1f,%6.1f,%d,%d\n\r",r,spd,arg,dash,T);
+	HAL_UART_Transmit(&huart2, (uint8_t*)test, 32,HAL_MAX_DELAY);
+
+	//ボタン押されて????��?��??��?��???��?��??��?��?な????��?��??��?��???��?��??��?��?ときに????��?��??��?��???��?��??��?��?????��?��??��?��???��?��??��?��?シュに1を代入して押されたときに2を代入
+	dash++;
+	if(dash==2)
+	{
+		spd=r/1.5;
+		if(spd>=1000)
+		{
+			spd=999;
+		}
+	}
+	else
+	{
+		spd=r/2;
+	}
+
+	if((113.0 >= arg) && (arg >= 68.0) && (r>100.0)){
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6,GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,GPIO_PIN_SET);
+		__HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_1,spd);
+		__HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_2,spd);
+		T=1;
+	}//pattern1/front
+	else if((68.0 >= arg) && (arg >= 23.0) && (r>100.0)){
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6,GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,GPIO_PIN_SET);
+		__HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_1,spd);
+		__HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_2,spd/2);
+		T=2;
+	}//pattern2/front right
+	else if((23.0 >= arg) && (arg >= -22.0) && (r>100.0)){
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6,GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,GPIO_PIN_RESET);
+		__HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_1,spd/3);
+		__HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_2,spd/3);
+		T=3;
+	}//pattern3/right rotation
+	else if((-22.0 >= arg) && (arg >= -67.0) && (r>100.0)){
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6,GPIO_PIN_SET);
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,GPIO_PIN_RESET);
+		__HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_1,spd);
+		__HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_2,spd/2);
+		T=4;
+	}//pattern4/back right
+	else if((-67.0 >= arg) && (arg >= -112.0) && (r>100.0)){
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6,GPIO_PIN_SET);
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,GPIO_PIN_RESET);
+		__HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_1,spd);
+		__HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_2,spd);
+		T=5;
+	}//pattern5/back
+	else if((-112.0 >= arg) && (arg >= -157.0) && (r>100.0)){
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6,GPIO_PIN_SET);
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,GPIO_PIN_RESET);
+		__HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_1,spd/2);
+		__HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_2,spd);
+		T=6;
+	}//pattern6/back left
+	else if(((158.0 <= arg) || (arg <= -157.0)) && (r>100.0)){
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6,GPIO_PIN_SET);
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,GPIO_PIN_SET);
+		__HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_1,spd/3);
+		__HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_2,spd/3);
+		T=7;
+	}//pattern7/left rotation
+	else if((158.0 >= arg) && (arg >= 113.0) && (r>100.0)){
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6,GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,GPIO_PIN_SET);
+		__HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_1,spd/2);
+		__HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_2,spd);
+		T=8;
+	}//pattern8/front left
+	else{
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6,GPIO_PIN_SET);
+		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,GPIO_PIN_RESET);
+		__HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_1,0);
+		__HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_2,0);
+		T=0;
+	}
+
+	  //???��?��??��?��?字�????��?��??��?��リセ???��?��??��?��?ト�????��?��??��?��処???��?��??��?��?
+	  if(strcmp(moji,check)==0)
+	  {
+		  reset++;
+	  }
+	  else
+	  {
+		  reset=0;
+		  memset(check,'\0',RX_BUFFER_SIZE);
+	  }
+	  if(reset>=25)
+	  {
+		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_RESET);
+		  memset(string,'\0',RX_BUFFER_SIZE);
+	  }
+	  else
+	  {
+		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_SET);
+	  }
+	  strcpy(check,moji);
+  }
+
+  HAL_Delay(10);
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
+  PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 7;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 999;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_PWM_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+  HAL_TIM_MspPostInit(&htim2);
+
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 38400;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 38400;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_11, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PA6 PA7 PA11 */
+  GPIO_InitStruct.Pin = GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_11;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB6 PB7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_6|GPIO_PIN_7;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF4_I2C1;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
